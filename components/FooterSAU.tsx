5ce2c0ce98a6f2882f793a04a521d88cb4d81a71
Bbolive <rapatsara.bam@gmail.com>
2026-04-08 14:17:38 +0700

    commit 0 : first page success

---
+++ components/FooterSAU.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function FooterSAU() {
+  return (
+    <div className='w-full text-center mt-10'>
+      <h3>
+        Created by Rapaatsara
+        <br />
+        Copyright &copy; 2026 All rights reserved
+      </h3>
+    </div>
+  )
+}
